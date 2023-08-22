61df8a0d961a698794b0195627c8c24fd0e9029c
bobbi <bobbi.thorndycraft@gmail.com>
2023-08-22 12:54:08 -0700

    Stage 4 - Everything Counts - Complete

--- task/src/sorting/InputAnalyzer.java
+++ task/src/sorting/InputAnalyzer.java
@@ -1,66 +1,85 @@
 package sorting;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class InputAnalyzer {
-    public static void analyzeLongInput(ArrayList<Long> userInput) {
-        long maxNum = Long.MIN_VALUE;
-        HashMap<Long, Integer> counts = new HashMap<>();
 
-        for (Long number : userInput) {
+    static class CountedEntry implements Comparable<CountedEntry>{
+        int count;
+        Object value;
+        DataType dataType;
 
-            //Only need to keep track of max count, so stop if this num isn't current max.
-            if (number >= maxNum) {
-                maxNum = number;
+        public CountedEntry(int count, Object value, DataType dataType) {
+            this.count = count;
+            this.value = value;
+            this.dataType = dataType;
+        }
 
-                if (counts.containsKey(number)) {
-                    counts.put(number, counts.get(number) + 1);
+        @Override
+        public int compareTo(CountedEntry other) {
+            if (this.count > other.count) {
+                return 1;
+            } else if (this.count == other.count) {
+                if (dataType == DataType.LONG) {
+                    return Long.compare((Long)this.value, (Long)other.value);
                 } else {
-                    counts.put(number, 1);
+                    return this.value.toString().compareTo(other.value.toString());
                 }
+            } else {
+                return -1;
             }
-        }
-
-        System.out.printf("Total numbers: %d.%n", userInput.size());
-        System.out.printf("The greatest number: %d (%d times(s)).", maxNum, counts.get(maxNum));
-    }
-
-    public static void analyzeStringInput(ArrayList<String> stringInput, DataType dataType) {
-        String longestString = "";
-        int stringCount = 0;
-        int longestStringCount = 0;
-        StringComparator comparator = new StringComparator();
-
-        for (String currentString : stringInput) {
-            if (currentString.equals(longestString)) {
-                longestStringCount++;
-            } else if (comparator.compare(currentString, longestString) > 0) {
-                longestString = currentString;
-                longestStringCount = 1;
-            }
-            stringCount++;
-        }
-
-        double longestStringPercentage = ((double) longestStringCount / stringCount) * 100;
-        System.out.printf("Total %ss: %d.%n", dataType.getName(), stringCount);
-
-        if (dataType == DataType.LINE) {
-            System.out.printf("""
-                    The longest line:
-                    %s
-                    (%d time(s), %.0f%%).
-                    """, longestString, longestStringCount, longestStringPercentage);
-        } else {
-            System.out.printf("The longest %s: %s (%d times(s), %.0f%%).",
-                    dataType.getName(), longestString, longestStringCount,longestStringPercentage );
         }
     }
 
-    public static void sortIntegers(ArrayList<Long> input) {
+    public static void sortStringsNatural(ArrayList<String> input, DataType dataType) {
+        input.sort(String::compareTo);
+
+        System.out.printf("Total %ss: %d%n", dataType.getName(), input.size());
+        System.out.print("Sorted data: ");
+
+        input.forEach(s -> {
+            System.out.printf("%s ", s);
+            if (dataType == DataType.LINE) {
+                System.out.println();
+            }
+        });
+    }
+
+    public static void sortLongsNatural(ArrayList<Long> input) {
+        input.sort(Long::compareTo);
+        StringBuilder sb = new StringBuilder();
+        input.forEach(num -> sb.append(num).append(" "));
         System.out.printf("Total numbers: %d.%n", input.size());
         System.out.print("Sorted data: ");
-        input.sort(Long::compareTo);
         input.forEach(num -> System.out.print(num + " "));
+    }
+
+    public static void sortByCount(Collection<? extends Object> input, DataType dataType) {
+        HashMap<Object,Integer> map = new HashMap<>();
+        ArrayList<CountedEntry> output = new ArrayList<>();
+
+        for (Object o : input) {
+             int keyCount = (map.containsKey(o)) ? map.get(o) + 1 : 1;
+             map.put(o, keyCount);
+        }
+
+        map.forEach((value, count) -> {
+                    CountedEntry thisEntry = new InputAnalyzer.CountedEntry(count, value, dataType);
+                    output.add(thisEntry);
+                });
+
+        output.sort(CountedEntry::compareTo);
+
+        printSortedData(output, dataType, input.size());
+    }
+
+    private static void printSortedData(ArrayList<CountedEntry> sortedList, DataType dataType, int totalCount) {
+        String typeString = dataType.equals(DataType.LONG) ? "number" : dataType.getName();
+
+        System.out.printf("Total %ss: %d%n", typeString, totalCount);
+        sortedList.forEach((CountedEntry entry) -> {
+            double percentage = ((double) entry.count / totalCount) * 100;
+            System.out.printf("%s: %d time(s), %.0f%%%n", entry.value.toString(), entry.count, percentage);
+        });
     }
 }
